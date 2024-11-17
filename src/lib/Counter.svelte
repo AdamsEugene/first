ae4fd52b1c6878ca385b098d8eb6240110bd5896
adams eugene <adamseugene292@gmail.com>
2024-11-17 00:34:48 +0000

    learning reactive statement

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,7 @@
+<script>
+	let count = $state(0);
+</script>
+
+<button class="rounded-lg bg-red-300 p-4 text-white" onclick={() => (count += 1)}
+	>Click {count}</button
+>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import Counter from '$lib/Counter.svelte';
+	import Image from '$lib/Image.svelte';
+
+	let { data }: { data: PageData } = $props();
+	let name = 'Adams';
+	let number = $state(1);
+
+	let numbers = $state([1, 2, 3, 4, 5]);
+	function getNumberName(index: number) {
+		let numberNames = [
+			'one',
+			'two',
+			'three',
+			'four',
+			'five',
+			'six',
+			'seven',
+			'eight',
+			'nine',
+			'ten'
+		];
+
+		return numberNames.at(index);
+	}
+
+	let enteredNumber = $derived(getNumberName(number - 1));
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h2>hello {name}</h2>
+<!-- <h1>{data.title}</h1>
+<div>{@html data.content}</div> -->
+<a href="/about">About my site</a>
+<ul>
+	{#each numbers as number}
+		<li>{getNumberName(number)}</li>
+	{/each}
+</ul>
+<input type="number" name="number" id="number" bind:value={number} max="10" min="1" />
+<p>You entered {enteredNumber}</p>
+<Counter />
+<Image height={50} width={90} />
